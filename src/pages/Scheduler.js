dcc9e73759f00299310e147ec5cbe6dc3f09eef4
Stephanie Irish Paladin <stephanieirishpaladin@gmail.com>
2024-03-26 21:39:20 -0600

    removed CourseContextProvider

--- src/pages/Scheduler.js
+++ src/pages/Scheduler.js
@@ -1,19 +1,16 @@
 import { CalendarPanel } from "../components/CalendarPanel";
 import { CoursesPanel } from "../components/CoursesPanel";
 import { RequirementsPanel } from "../components/RequirementsPanel";
-import { CourseContextProvider } from "../context/CourseContext";
 import { RequirementsContextProvider } from "../context/RequirementsContext";
 
 const Scheduler = () => {
     return (
         <RequirementsContextProvider>
-            <CourseContextProvider>
-                <div className="pages scheduler-page">
-                    <RequirementsPanel />
-                    <CoursesPanel />
-                    <CalendarPanel />
-                </div>
-            </CourseContextProvider>
+            <div className="pages scheduler-page">
+                <RequirementsPanel />
+                <CoursesPanel />
+                <CalendarPanel />
+            </div>
         </RequirementsContextProvider>
     )
 };
