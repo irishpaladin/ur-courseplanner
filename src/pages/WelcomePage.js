02ded4bfd8a60938b2789be8aa7f1862cd3dea5a
Stephanie Irish Paladin <stephanieirishpaladin@gmail.com>
2024-03-27 00:00:26 -0600

    Remove unused css design

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -6,23 +6,19 @@ import rightImage from "../assets/homepage-image.png";
 const WelcomePage = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  return (
-    <div className="app-container">
+  return !isAuthenticated && (
+    <div className="app-container welcome-page">
       <div className="main-content">
         <img src={leftImage} alt="Top Left Image" className="top-left-image" style={{width: "50%", marginRight: "20%"}}/>
         <div className="maindescription">
-        <h1 className="title" style={{fontSize: 44}}><b>Easy Planning</b></h1>
+        <h1 className="title" style={{fontSize: "80px"}}><b>Easy Planning</b></h1>
         <p className="description" style={{marginTop: '20px', textAlign: 'left'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan. lacus vel facilisis.</p>
-        {!isAuthenticated && (
           <button className="get-started-button" onClick={() => loginWithRedirect()}>
             GET STARTED
           </button>
-        )}
-        {!isAuthenticated && (
           <p className="already-account-text">
             Already have an account? <span className="sign-in-text" onClick={() => loginWithRedirect()} style={{color: 'blue', cursor: 'pointer'}}>Sign In</span>
           </p>
-        )}
         </div>
       </div>
       <div className="right-section">
