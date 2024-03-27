66bebd9376ae76737b844d62f18d189c0161b3b9
Stephanie Irish Paladin <stephanieirishpaladin@gmail.com>
2024-03-26 21:36:06 -0600

    Added mock data; fucntion to fetch data

---
+++ src/context-httprequest/FetchAllCourseData.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export function FetchAllCourseData() {
+    const [data, setData] = useState([]);
+    //HTTP request to save/retrieve useris from database
+    useEffect(() => {
+        console.log('FetchingAllCoursesData...');
+        fetch(`https://urcourseplannerbff.azurewebsites.net/Course/All`)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+                setData({ data })
+            })
+            .catch(error => {
+                console.error('Error All Courses data!', error);
+            });
+    }, []);
+    return (data);
+}
