b11eb99d3db2dd820360b38caf6a2c9cc58e74aa
Falgun <falgunpatel756@gmail.com>
2024-03-26 23:32:51 -0600

    Welcome page design

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './fonts/Lato/Lato-Regular.ttf';
 import './fonts/Lato/Lato-Bold.ttf';
-
+import './styles.css';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,18 +1,35 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import leftImage from "../assets/icon.png";
+import rightImage from "../assets/homepage-image.png";
 
 const WelcomePage = () => {
-    const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-    return (
-        !isAuthenticated && (
-            <div>
-                <h1>This is WelcomePage</h1>
-                <button onClick={() => loginWithRedirect()}>
-                    Sign In
-                </button>
-            </div>
-        )
-    )
+  return (
+    <div className="app-container">
+      <div className="main-content">
+        <img src={leftImage} alt="Top Left Image" className="top-left-image" style={{width: "50%", marginRight: "20%"}}/>
+        <div className="maindescription">
+        <h1 className="title" style={{fontSize: 44}}><b>Easy Planning</b></h1>
+        <p className="description" style={{marginTop: '20px', textAlign: 'left'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan. lacus vel facilisis.</p>
+        {!isAuthenticated && (
+          <button className="get-started-button" onClick={() => loginWithRedirect()}>
+            GET STARTED
+          </button>
+        )}
+        {!isAuthenticated && (
+          <p className="already-account-text">
+            Already have an account? <span className="sign-in-text" onClick={() => loginWithRedirect()} style={{color: 'blue', cursor: 'pointer'}}>Sign In</span>
+          </p>
+        )}
+        </div>
+      </div>
+      <div className="right-section">
+        <img src={rightImage} alt="Right Side Image" className="right-side-image" />
+      </div>
+    </div>
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
